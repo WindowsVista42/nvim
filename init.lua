58085c9aedeb8cc67e44d72672eb424de4b0b924
Sean Moulton <34073738+WindowsVista42@users.noreply.github.com>
2022-03-20 23:43:48 -0500

    Wrap install path in quotes because spaces can exist in the path

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ require('settings')
 -- Auto install packer.nvim if it does not exist
 local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..'"'..install_path..'"')
 end
 vim.cmd [[packadd packer.nvim]]
 vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
